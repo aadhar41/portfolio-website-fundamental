07d0874f4bbd698bba102d0efc7c7d63e718089f
Aadhar gaur <aadhar41@gmail.com>
2023-10-08 12:59:56 +0530

    21 - Conditional Rendering

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,22 @@
     <h2>Portfolio</h2>
     <div class="row">
         @foreach ($projects as $project)
-        <!-- Portfolio Item -->
-        <div class="col-lg-4 col-md-6 portfolio-item">
-            <img src="{{ $project['image'] }}" alt="{{ $project['name'] }}">
-            <h3>{{ $project['name'] }}</h3>
-            <p>{{ $project['description'] }}</p>
-        </div>
+            @if ($project['status'] == 1)
+                <!-- Portfolio Item -->
+                <div class="col-lg-4 col-md-6 portfolio-item">
+                    <img src="{{ $project['image'] }}" alt="{{ $project['name'] }}">
+                    <h3>{{ $project['name'] }}</h3>
+                    <p>{{ $project['description'] }}</p>
+                </div>
+            @else
+                <!-- Portfolio Item -->
+                <div class="col-lg-4 col-md-6 portfolio-item">
+                    <img src="{{ $project['image'] }}" alt="{{ $project['name'] }}">
+                    <h3>{{ $project['name'] }}</h3>
+                    <p>{{ $project['description'] }}</p>
+                    <p><button class="btn btn-warning" disabled="disabled">Pending...</button></p>
+                </div>
+            @endif
         @endforeach
     </div>
 </section>
