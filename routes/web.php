f623b55401e43028743c9287ddf46f4189cc5899
Aadhar gaur <aadhar41@gmail.com>
2023-10-25 23:18:24 +0530

    19-Fundamental-Sending-Email

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
+use App\Mail\OrderShipped;
+use App\Models\Post;
+use Illuminate\Support\Facades\Mail;
 
 use function PHPUnit\Framework\callback;
 
@@ -42,5 +45,21 @@ Route::post('/login', [LoginController::class, 'handleLogin'])->name('login.subm
     Route::resource('posts', PostController::class);
 // });
 
+
+Route::get('send-mail', function() {
+    // Mail::raw('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec urna eget dolor semper posuere. Sed at purus nec nulla ullamcorper mattis.', function($message) {
+    //     $message->to('aadhar41@gmail.com')->subject('Testing E-Mail');
+    // });
+
+    
+    $post = App\Models\Post::find(1);
+    // return new App\Mail\OrderShipped($subject = 'Testing HTML', $title = "Hi E-Mail", $content = "Lorem ipsum", $buttonText = 'Click Here', $buttonUrl = '#', $email = 'rs12732@gmail.com', $post);
+    // return new App\Mail\OrderShipped($post);
+    Mail::send(new App\Mail\OrderShipped($post));
+    // return Mail::send(new OrderShipped($post));
+
+
+    dd('Success!! Mail Sent.');
+});
 
 
